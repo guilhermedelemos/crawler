24c902527e134249f779fb6a7478854493d2f9f4
Guilherme de Lemos <guilherme.eti@gmail.com>
2019-07-13 18:54:12 -0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  mode: "production",
   entry: './src/index.js',
   output: {
+    library: 'crawler',
     filename: 'crawler.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  target: "web",
 };
